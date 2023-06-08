859acabc8b8e8e8e1c9ea8eb6a2084007cad7b4e
Sohom Pal <sohom377@gmail.com>
2023-06-08 14:47:03 +0530

    completion of hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { logo } from '../assets';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt="sumz_logo" className='w-28 object-contain'/>
+
+        <button type="button" onClick={() => window.open('https://github.com/sohomx')}
+        className="black_btn">
+          Github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className='max-md:hidden'/>
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+
+      <h2 className='desc'>Simply your reading links, an open-source article summarizer that transforms lengthy articles into clear and consise summaries</h2>
+
+    </header>
   )
 }
 
