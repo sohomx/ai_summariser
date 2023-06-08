43b8a4dbe319753571fd56af10d8fcf6db0ce4bc
Sohom Pal <sohom377@gmail.com>
2023-06-09 00:20:54 +0530

    fetching the summarisation of the link is broken

--- src/services/article.js
+++ src/services/article.js
@@ -1,5 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const rapidAPIKey = import.meta.env.VITE_RAPID_ARTICLE_API_KEY;
+  
 export const articleApi = createApi({
     reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', rapidAPIKey   );
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+            return headers;
+        }
+    }),
+    endpoints: (builder) => ({
+        getSummary: builder.query({
+            getSummary: (params) => 'test'
+        })
+    })
 })
